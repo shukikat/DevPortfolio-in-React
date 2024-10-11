5193f268ce1226d4465d95fa2ee04ad0edee4c73
shukikat <kathuriashuki@gmail.com>
2024-10-11 18:00:02 -0400

    added inages to Portfolio

--- vite-project/src/components/Portfolio.jsx
+++ vite-project/src/components/Portfolio.jsx
@@ -1,4 +1,9 @@
-//import Header from './Header'
+import {Project1} from './Project'
+import {Project2} from './Project'
+import {Project3} from './Project'
+import {Project4} from './Project'
+import {Project5} from './Project'
+
 
 
 export default function Portfolio() {
@@ -9,6 +14,12 @@ export default function Portfolio() {
 
 
             <h1>Portfolio</h1>
+            <div><Project1/></div>
+            <div>{Project2}</div>
+            <div>{Project3}</div>
+            <div>{Project4}</div>
+            <div>{Project5}</div>
+
 
 
         </>
---
+++ vite-project/src/components/Project.jsx
@@ -0,0 +1,64 @@
+
+import springImage from '../images/spring.jpg';
+import meditation from '../images/meditation.jpg';
+import foliage from '../images/foliage.png';
+import horse from '../images/horse.jpg';
+import random from '../images/background.jpg';
+
+function Project1() {
+    return (
+        <img src={springImage} alt='Spring'/>
+    );
+    
+}
+
+function Project2() {
+    return (
+        <img src={meditation} alt='Meditation'/>
+    );
+    
+}
+
+
+function Project3() {
+    return (
+        <img src={foliage} alt='Foliage'/>
+
+    
+    );
+
+    
+}
+
+function Project4() {
+    return (
+        <img src={horse} alt='Horse'/>
+
+    
+    );
+
+    
+}
+
+function Project5() {
+    return (
+        <img src={random} alt='Random'/>
+
+    
+    );
+
+    
+}
+
+
+
+
+
+
+
+
+export {Project1}
+export{Project2}
+export{Project3}
+export{Project4}
+export{Project5}
